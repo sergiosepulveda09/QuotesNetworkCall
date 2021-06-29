04b9467e2110e76abf69eea21cc4083dc71a58d7
Sergio <sergiosepulveda09@live.com>
2021-06-28 18:09:07 -0700

    Initial Commit

---
+++ ProgrammingQuotesNetworkCall/ProgrammingQuotesNetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammingQuotesNetworkCallApp.swift
+//  ProgrammingQuotesNetworkCall
+//
+//  Created by Sergio Sepulveda on 2021-06-28.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammingQuotesNetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
